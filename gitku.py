2e746a07ee1374e3b9fea212159566d5bd86cbc8
zahiruddin <kepaknaga@gmail.com>
2020-09-13 08:02:09 +0800

    update 2.0

--- gitku.py
+++ gitku.py
@@ -1,19 +1,21 @@
 import os
+# kepaknaga@gmail.com
 
 def cuci():
 	os.system('clear')
 
 cuci()
 while True:
-	print('===============')
-	print('=====GITKU=====')
+	print('====================')
+	print('=====GITKU v2.0=====')
 	print(' 0 = git pull')
 	print(' 1 = git add .')
 	print(' 2 = git commit -m')
 	print(' 3 = git push')
 	print(' 4 = git add & commit')
-	print(' 5 = git init')
+	print(' 5 = git diff')
 	print(' 6 = git status')
+	print(' 7 = git checkout .')
 	print('19 = exit')
 
 	jwp = input('Ans: ')
@@ -29,8 +31,9 @@ while True:
 		os.system("git commit -m '" + note + "'")
 
 	elif jwp == "3":
+		note = input("branch : ")
 		cuci()
-		os.system('git push')
+		os.system('git push -u origin ' + note)
 
 	elif jwp == "4":
 		cuci()
@@ -41,11 +44,19 @@ while True:
 
 	elif jwp == "5":
 		cuci()
-		os.system('git init')
+		os.system('git diff')
 
 	elif jwp == "6":
 		cuci()
 		os.system('git status')
+
+	elif jwp == "7":
+		note = input("sure (y/n) : ")
+		if note is 'y':
+			cuci()
+			os.system('git checkout .')
+		else:
+			cuci()
 
 	elif jwp == "0":
 		cuci()
